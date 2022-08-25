9128c7569d86656734ab2f896df9652516814df9
therencedemeyer <irisib>
2022-08-25 17:34:58 +0200

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBkOLVgma44UyuqbmLqgekIfPB2ypI4dX0',
+    authDomain: 'photoapp-c6631.firebaseapp.com',
+    projectId: 'photoapp-c6631',
+    storageBucket: 'photoapp-c6631.appspot.com',
+    messagingSenderId: '975429759630',
+    appId: '1:975429759630:web:812192a07c53f255cb8fdf',
+    measurementId: 'G-74GTLL01YE'
+  }
 };
 
 /*
